7ade11693b19d98e9de4d022e2ddcd961dadd4a4
Abhi <meabhishek1512@gmail.com>
2024-02-18 15:31:56 +0530

    #04 (i) Created E-commerce data model using Mongoose

---
+++ 04-ecommerce-and-hospital-mngmt-data-modelling/models/ecommerce/user.models.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+// Created user database schema
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+export const User = mongoose.model("User", userSchema)
